2dc1bf05f2e1953b68f285e3297bf901bc9a5645
AndreyS <andreysx@tut.by>
2017-02-15 14:10:40 +0300

    Initial commit

---
+++ js/jQuery.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Andrey on 12-Feb-17.
+ */
+
+$(function () {
+    $('#fS').click(function () {
+        $('form').slideToggle('1500');
+    });
+
+    $('#cancel').click(function () {
+        $('form').slideUp('1500');
+    });
+    $('article').css('display', 'none');
+    $('article').fadeIn(1200);
+    $('.ddImg').css('text-align', 'left')
+    $('article.main img').css({
+        'width': '0'
+    });
+    $('article.main img').animate({
+        'width': '200px'
+    }, 500);
+});
+
+
